ed9356b0cd60c434c9a3e75341f7ce53d82af74f
idubi.m <idubi.m@gmail.com>
2024-03-26 05:14:40 +0200

    fix ip

--- bash/terraform_utils.sh
+++ bash/terraform_utils.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 get_load_balance_ip(){
-    lbip=$(terraform output |grep -oP 'lb_dns_name = "\K[^"]+')
-    echo $lbip
+    output=$(terraform output)
+    lb_dns_name=$(echo "$output" | grep lb_dns_name | cut -d'=' -f2 | tr -d ' "')
+    lb_ip_address=$(dig +short $lb_dns_name)
 }
