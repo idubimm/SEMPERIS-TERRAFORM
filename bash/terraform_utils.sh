d8ff22e5e059679837577df6df22d97390e269dd
idubi.m <idubi.m@gmail.com>
2024-03-26 05:48:45 +0200

    utils changed

--- bash/terraform_utils.sh
+++ bash/terraform_utils.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 get_load_balance_ip(){
-    output=$(terraform output)
-    lb_dns_name=$(echo "$output" | grep lb_dns_name | cut -d'=' -f2 | tr -d ' "')
-    lb_ip_address=$(dig +short $lb_dns_name)
+    lbip=$(terraform output |grep -oP 'lb_dns_name = "\K[^"]+')
+    echo $lbip
 }
